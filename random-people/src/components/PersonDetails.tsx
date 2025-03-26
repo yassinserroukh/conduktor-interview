460328c9e56da0c956915d6230d61012d36812cc
Yassin Serroukh <yassinserroukh00@gmail.com>
2025-03-26 16:21:20 +0000

    Person details modal

---
+++ random-people/src/components/PersonDetails.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Person, PersonRow } from "../types.ts";
+
+interface PersonDetailsProps {
+  columns: PersonRow[];
+  personalDetails: Person;
+  setShowPersonRow: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const PersonDetails: React.FC<PersonDetailsProps> = ({
+  columns,
+  personDetails,
+  setShowPersonRow,
+}: PersonDetailsProps) => {
+  return (
+    <div>
+      <div className="modal-container">
+        <div className="modal-content border rounded shadow-sm w-25">
+          <div className="modal-body">
+            <ul className="list-group list-group-flush">
+              {columns.map((header) => (
+                <li
+                  key={header.id}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  <span className="fw-bold">{header.label}</span>
+                  <span className="text-muted">
+                    {personDetails?.[header.key]}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="modal-footer">
+            <button
+              className="btn btn-secondary"
+              onClick={() => setShowPersonRow(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
