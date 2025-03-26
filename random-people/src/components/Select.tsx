cc6d7c5710c3454b28ceec9e16dc62694f099a88
Yassin Serroukh <yassinserroukh00@gmail.com>
2025-03-26 16:21:07 +0000

    Select component

---
+++ random-people/src/components/Select.tsx
@@ -0,0 +1,24 @@
+import React, { ChangeEventHandler } from "react";
+
+interface SelectProps {
+  value: string;
+  actionHandler: ChangeEventHandler<HTMLSelectElement>;
+}
+
+export const Select: React.FC<SelectProps> = ({
+  value,
+  actionHandler,
+}: SelectProps) => {
+  return (
+    <div className="number-of-records">
+      <label>Choose number of records per page: </label>{" "}
+      <select className="form-select-sm" value={value} onChange={actionHandler}>
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
+    </div>
+  );
+};
